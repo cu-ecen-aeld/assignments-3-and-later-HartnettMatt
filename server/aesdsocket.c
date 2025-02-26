89c42a3cd9979cb8c623dd67ccb123e96f579436
Matt Hartnett <matthew.e.hartnett@gmail.com>
2025-02-25 22:41:15 -0700

    Got most of the way through part 1, stuck on infinite waiting for aesdsocket application to start

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -20,6 +20,8 @@
  *
  *****************************************************************************/
 
+#include <pthread.h>
+#include <sys/queue.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -35,13 +37,37 @@
 #include <arpa/inet.h>
 #include <sys/stat.h>
 
+// Copied from BSD queue.h code https://github.com/freebsd/freebsd-src/blob/main/sys/sys/queue.h
+#define	SLIST_FOREACH_SAFE(var, head, field, tvar)			\
+	for ((var) = SLIST_FIRST((head));				\
+	    (var) && ((tvar) = SLIST_NEXT((var), field), 1);		\
+	    (var) = (tvar))
+
 #define PORT 9000
 #define BACKLOG 10
 #define DATA_FILE "/var/tmp/aesdsocketdata"
 #define READ_BUFFER_SIZE 1024
 
+// Timestamping thread
+pthread_t timestamp_thread;
+
 // Global flag set by signal handlers to indicate termination requested
 volatile sig_atomic_t terminate_flag = 0;
+
+// Mutex to synchronize file writes (and associated reads)
+pthread_mutex_t file_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Structure to hold thread information:
+typedef struct thread_info_t {
+    pthread_t thread_id;
+    int client_fd;
+    struct sockaddr_in client_addr;
+    bool complete;
+    SLIST_ENTRY(thread_info_t) entries;
+} thread_info_t;
+
+// Linked list head
+SLIST_HEAD(slisthead, thread_info_t) thread_list_head =     SLIST_HEAD_INITIALIZER(thread_list_head);
 
 // Signal handler: set flag and log the event
 static void signal_handler(int signo)
@@ -67,33 +93,6 @@ static int setup_signal_handlers(void)
     return 0;
 }
 
-// Helper function to send the entire contents of DATA_FILE to client_fd
-static int send_file_to_client(int client_fd)
-{
-    FILE *fp = fopen(DATA_FILE, "r");
-    if (fp == NULL) {
-        perror("fopen for reading");
-        return -1;
-    }
-    char file_buf[READ_BUFFER_SIZE];
-    size_t bytes_read;
-    while ((bytes_read = fread(file_buf, 1, sizeof(file_buf), fp)) > 0) {
-        size_t bytes_sent = 0;
-        while (bytes_sent < bytes_read) {
-            ssize_t rc = send(client_fd, file_buf + bytes_sent,
-                              bytes_read - bytes_sent, 0);
-            if (rc < 0) {
-                perror("send");
-                fclose(fp);
-                return -1;
-            }
-            bytes_sent += rc;
-        }
-    }
-    fclose(fp);
-    return 0;
-}
-
 // Helper function to perform cleanup
 void cleanup(int sockfd)
 {
@@ -104,6 +103,178 @@ void cleanup(int sockfd)
         perror("remove");
     }
     closelog();
+}
+
+// Thread function to process connection
+void *connection_handler(void *arg)
+{
+    // Pull client info from arg
+    thread_info_t *tinfo = (thread_info_t *)arg;
+    int client_fd = tinfo->client_fd;
+    struct sockaddr_in client_addr = tinfo->client_addr;
+
+    // Process connection (used to be in main):
+    // Log the accepted connection
+    char client_ip[INET_ADDRSTRLEN];
+    if (inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip)) != NULL) {
+        syslog(LOG_INFO, "Accepted connection from %s", client_ip);
+    } else {
+        syslog(LOG_INFO, "Accepted connection from unknown client");
+    }
+
+    // Process the connection
+    char recv_buf[READ_BUFFER_SIZE];
+    char *incomplete_buf = NULL;
+    size_t incomplete_buf_len = 0;
+    bool connection_error = false;
+
+    while (!terminate_flag) {
+        ssize_t bytes_rcvd = recv(client_fd, recv_buf, sizeof(recv_buf), 0);
+        if (bytes_rcvd < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            perror("recv");
+            connection_error = true;
+            break;
+        } else if (bytes_rcvd == 0) {
+            // Client closed connection
+            break;
+        }
+
+        // Append new data to the buffer
+        char *temp = realloc(incomplete_buf, incomplete_buf_len + bytes_rcvd);
+        if (!temp) {
+            perror("realloc");
+            free(incomplete_buf);
+            incomplete_buf = NULL;
+            connection_error = true;
+            break;
+        }
+        incomplete_buf = temp;
+        memcpy(incomplete_buf + incomplete_buf_len, recv_buf, bytes_rcvd);
+        incomplete_buf_len += bytes_rcvd;
+
+        // Process complete packets (lines ending with newline)
+        char *line_start = incomplete_buf;
+        char *newline_ptr = NULL;
+        while ((newline_ptr = memchr(line_start, '\n',
+                      (incomplete_buf + incomplete_buf_len) - line_start))) {
+            size_t line_len = newline_ptr - line_start + 1;
+            // Lock the mutex to synchronize file access
+            pthread_mutex_lock(&file_mutex);
+            // Append packet to file
+            FILE *fp = fopen(DATA_FILE, "a");
+            if (fp == NULL) {
+                perror("fopen for append");
+                pthread_mutex_unlock(&file_mutex);
+                connection_error = true;
+                break;
+            }
+            if (fwrite(line_start, 1, line_len, fp) != line_len) {
+                perror("fwrite");
+                fclose(fp);
+                pthread_mutex_unlock(&file_mutex);
+                connection_error = true;
+                break;
+            }
+            fclose(fp);
+            // Send the file contents back to the client
+            fp = fopen(DATA_FILE, "r");
+            if (fp == NULL) {
+                perror("fopen for reading");
+                pthread_mutex_unlock(&file_mutex);
+                connection_error = true;
+                break;
+            }
+            char file_buf[READ_BUFFER_SIZE];
+            size_t bytes_read;
+            while ((bytes_read = fread(file_buf, 1, sizeof(file_buf), fp)) > 0) {
+                size_t bytes_sent = 0;
+                while (bytes_sent < bytes_read) {
+                    ssize_t rc = send(client_fd, file_buf + bytes_sent,
+                                      bytes_read - bytes_sent, 0);
+                    if (rc < 0) {
+                        perror("send");
+                        fclose(fp);
+                        pthread_mutex_unlock(&file_mutex);
+                        connection_error = true;
+                        break;
+                    }
+                    bytes_sent += rc;
+                }
+            }
+            fclose(fp);
+            pthread_mutex_unlock(&file_mutex);
+            line_start = newline_ptr + 1;
+        }
+        if (connection_error) {
+            break;
+        }
+
+        // Remove processed data from the buffer
+        size_t remaining = (incomplete_buf + incomplete_buf_len) - line_start;
+        if (remaining > 0) {
+            memmove(incomplete_buf, line_start, remaining);
+        }
+        incomplete_buf_len = remaining;
+        if (remaining == 0) {
+            free(incomplete_buf);
+            incomplete_buf = NULL;
+        } else {
+            char *temp2 = realloc(incomplete_buf, remaining);
+            if (temp2)
+                incomplete_buf = temp2;
+        }
+    }
+
+    free(incomplete_buf);
+    incomplete_buf = NULL;
+    close(client_fd);
+
+    // Log connection closure
+    if (inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip)) != NULL) {
+        syslog(LOG_INFO, "Closed connection from %s", client_ip);
+    } else {
+        syslog(LOG_INFO, "Closed connection from unknown client");
+    }
+
+    tinfo->complete = true;
+    return NULL;
+}
+
+
+// Timestamping thread function
+void *timestamp_thread_func(void *arg)
+{
+    while (!terminate_flag) {
+        sleep(10);  // Wait 10 seconds
+        time_t now = time(NULL);
+        struct tm *tm_info = localtime(&now);
+        if (!tm_info) {
+            perror("localtime");
+            continue;
+        }
+        char time_str[128];
+        if (strftime(time_str, sizeof(time_str), "%a, %d %b %Y %H:%M:%S %z", tm_info) == 0) {
+            perror("strftime returned 0");
+            continue;
+        }
+        char timestamp[256];
+        snprintf(timestamp, sizeof(timestamp), "timestamp:%s\n", time_str);
+
+        // Use mutex to ensure atomic write with respect to socket data
+        pthread_mutex_lock(&file_mutex);
+        FILE *fp = fopen(DATA_FILE, "a");
+        if (fp) {
+            fwrite(timestamp, 1, strlen(timestamp), fp);
+            fclose(fp);
+        } else {
+            perror("fopen in timestamp thread");
+        }
+        pthread_mutex_unlock(&file_mutex);
+    }
+    return NULL;
 }
 
 int main(int argc, char *argv[])
@@ -190,8 +361,26 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    // Start timestamp thread to run in the background
+    if (pthread_create(&timestamp_thread, NULL, timestamp_thread_func, NULL) != 0) {
+        perror("pthread_create for timestamp thread");
+        cleanup(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
     // Main loop: accept and process connections until termination is requested
     while (!terminate_flag) {
+        // Check for completed threads before accepting a new connection
+        thread_info_t *cur = NULL;
+        thread_info_t *temp = NULL;
+        SLIST_FOREACH_SAFE(cur, &thread_list_head, entries, temp) {
+            if (cur->complete) {
+                pthread_join(cur->thread_id, NULL);
+                SLIST_REMOVE(&thread_list_head, cur, thread_info_t, entries);
+                free(cur);
+            }
+        }
+
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
         client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
@@ -203,105 +392,40 @@ int main(int argc, char *argv[])
             continue;
         }
 
-        // Log the accepted connection
-        char client_ip[INET_ADDRSTRLEN];
-        if (inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip)) != NULL) {
-            syslog(LOG_INFO, "Accepted connection from %s", client_ip);
-        } else {
-            syslog(LOG_INFO, "Accepted connection from unknown client");
+        // Allocate a new thread for the connection and add client info
+        thread_info_t *tinfo = malloc(sizeof(thread_info_t));
+        if (!tinfo) {
+            perror("malloc thread_info");
+            close(client_fd);
+            continue;
+        }
+        tinfo->client_fd = client_fd;
+        memcpy(&tinfo->client_addr, &client_addr, sizeof(client_addr));
+
+        // Create a thread to handle this connection
+        int ret = pthread_create(&tinfo->thread_id, NULL, connection_handler, tinfo);
+        if (ret != 0) {
+            errno = ret;
+            perror("pthread_create");
+            close(client_fd);
+            free(tinfo);
+            continue;
         }
 
-        // Process the connection
-        char recv_buf[READ_BUFFER_SIZE];
-        char *incomplete_buf = NULL;
-        size_t incomplete_buf_len = 0;
-        bool connection_error = false;
-
-        while (!terminate_flag) {
-            ssize_t bytes_rcvd = recv(client_fd, recv_buf, sizeof(recv_buf), 0);
-            if (bytes_rcvd < 0) {
-                if (errno == EINTR) {
-                    continue;
-                }
-                perror("recv");
-                connection_error = true;
-                break;
-            } else if (bytes_rcvd == 0) {
-                // Client closed connection
-                break;
-            }
-
-            // Append new data to the buffer
-            char *temp = realloc(incomplete_buf, incomplete_buf_len + bytes_rcvd);
-            if (!temp) {
-                perror("realloc");
-                free(incomplete_buf);
-                incomplete_buf = NULL;
-                connection_error = true;
-                break;
-            }
-            incomplete_buf = temp;
-            memcpy(incomplete_buf + incomplete_buf_len, recv_buf, bytes_rcvd);
-            incomplete_buf_len += bytes_rcvd;
-
-            // Process complete packets (lines ending with newline)
-            char *line_start = incomplete_buf;
-            char *newline_ptr = NULL;
-            while ((newline_ptr = memchr(line_start, '\n',
-                          (incomplete_buf + incomplete_buf_len) - line_start))) {
-                size_t line_len = newline_ptr - line_start + 1;
-                FILE *fp = fopen(DATA_FILE, "a");
-                if (fp == NULL) {
-                    perror("fopen for append");
-                    connection_error = true;
-                    break;
-                }
-                if (fwrite(line_start, 1, line_len, fp) != line_len) {
-                    perror("fwrite");
-                    fclose(fp);
-                    connection_error = true;
-                    break;
-                }
-                fclose(fp);
-                if (send_file_to_client(client_fd) != 0) {
-                    connection_error = true;
-                    break;
-                }
-                line_start = newline_ptr + 1;
-            }
-            if (connection_error) {
-                break;
-            }
-
-            // Remove processed data from the buffer
-            size_t remaining = (incomplete_buf + incomplete_buf_len) - line_start;
-            if (remaining > 0) {
-                memmove(incomplete_buf, line_start, remaining);
-            }
-            incomplete_buf_len = remaining;
-            if (remaining == 0) {
-                free(incomplete_buf);
-                incomplete_buf = NULL;
-            } else {
-                char *temp2 = realloc(incomplete_buf, remaining);
-                if (temp2)
-                    incomplete_buf = temp2;
-            }
-        }
-
-        free(incomplete_buf);
-        incomplete_buf = NULL;
-        close(client_fd);
-
-        // Log connection closure
-        if (inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip)) != NULL) {
-            syslog(LOG_INFO, "Closed connection from %s", client_ip);
-        } else {
-            syslog(LOG_INFO, "Closed connection from unknown client");
-        }
+        // Add thread to linked list
+        SLIST_INSERT_HEAD(&thread_list_head, tinfo, entries);
     }
 
-    // Final cleanup and exit
+    // Final cleanup: join any remaining threads, exit
+    thread_info_t *cur = NULL;
+    thread_info_t *temp = NULL;
+    SLIST_FOREACH_SAFE(cur, &thread_list_head, entries, temp) {
+        pthread_join(cur->thread_id, NULL);
+        SLIST_REMOVE(&thread_list_head, cur, thread_info_t, entries);
+        free(cur);
+    }
+    // End timestamp thread
+    pthread_join(timestamp_thread, NULL);
     cleanup(sockfd);
     return 0;
 }
